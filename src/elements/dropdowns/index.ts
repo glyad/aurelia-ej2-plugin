b5c150634d4b7f2f6fd15302a97c807426475cdc
David Kossoglyad <david@logoui.co.il>
2020-02-05 13:07:42 +0200

    fix: global resources

--- src/elements/dropdowns/index.ts
+++ src/elements/dropdowns/index.ts
@@ -11,8 +11,6 @@ export * from './list-box/index';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    PLATFORM.moduleName('./auto-complete/ej-auto-complete'),
-    PLATFORM.moduleName('./drop-down-base/ej-drop-down-base'),    
-    PLATFORM.moduleName('./drop-down-list/ej-drop-down-list')
+    
   ]);
 }
--- src/elements/index.ts
+++ src/elements/index.ts
@@ -7,7 +7,11 @@ export * from './inputs/index';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
+    PLATFORM.moduleName('./elements/ej-button'),
+    PLATFORM.moduleName('./elements/ej-checkbox'),
     PLATFORM.moduleName('./elements/ej-radio-button'),
+    PLATFORM.moduleName('./elements/dropdowns/auto-complete/ej-auto-complete'),
+    PLATFORM.moduleName('./elements/dropdowns/drop-down-list/ej-drop-down-list')
   ]);
 
 }
