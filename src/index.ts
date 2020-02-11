00224e60dcfe6e6b6cb3f5fdacca4ef2750848e6
David Kossoglyad <david@logoui.co.il>
2020-02-11 14:28:04 +0200

    feat: register newly added components

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,18 @@ export function configure(config: FrameworkConfiguration) {
     PLATFORM.moduleName('./elements/buttons/button/ej-button'),
     PLATFORM.moduleName('./elements/buttons/check-box/ej-checkbox'),
     PLATFORM.moduleName('./elements/buttons/radio-button/ej-radio-button'),
+    PLATFORM.moduleName('./elements/buttons/switch/ej-switch'),
+    PLATFORM.moduleName('./elements/inputs/textbox/ej-textbox'),
+    PLATFORM.moduleName('./elements/inputs/maskedtextbox/ej-masked-textbox'),
+    PLATFORM.moduleName('./elements/inputs/numerictextbox/ej-numerictextbox'),
     PLATFORM.moduleName('./elements/dropdowns/auto-complete/ej-auto-complete'),
     PLATFORM.moduleName('./elements/dropdowns/drop-down-list/ej-drop-down-list')
-  ]);
+  ])
+  // .aurelia.use
+  // .plugin('aurelia-highlightjs', config => config
+  //      .useLanguage("bash")
+  //      .useLanguage("javascript")
+  //    );
+    ;
 
 }
