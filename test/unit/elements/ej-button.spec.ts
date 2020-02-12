285bb67300dde89d4f011171a64e5386f217c57b
David Kossoglyad <david@logoui.co.il>
2020-02-12 12:22:18 +0200

    experiments with tests

--- test/unit/elements/ej-button.spec.ts
+++ test/unit/elements/ej-button.spec.ts
@@ -2,6 +2,7 @@ import { EjButtonModel } from './../../../src/elements/buttons/button/ej-button-
 import {StageComponent} from 'aurelia-testing';
 import {bootstrap} from 'aurelia-bootstrapper';
 import { ButtonModel } from '@syncfusion/ej2-buttons';
+import { stringify } from 'querystring';
 
 
 let model = {
@@ -15,6 +16,24 @@ let model = {
   toggle: false,
 };
 
+function toKebabCase(str: string) {
+  str &&
+  str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map(x => x.toLowerCase())
+    .join('-');
+}
+
+function buildBindingsString(model: {}) {
+  let result: string = '';
+
+  Object.keys(model).forEach(key => {
+    result += ` ${toKebabCase(key)}.bind="${key}" `;
+  });
+
+  return result;
+}
+
 describe('ej-button element', () => {
   let component;
 
@@ -25,37 +44,60 @@ describe('ej-button element', () => {
     }
   });
 
-  it('binds the Content property', done => {
+  Object.keys(model).forEach(key => {
+    console.log(`${key} = ${model[key]}`);
 
-    component = StageComponent
-      .withResources('resources/elements/buttons/button/ej-button')
-      .inView('<ej-button content.bind="content"></ej-button>')
-      .boundTo(model);
+    it(`binds the ${key} property`, done => {
 
-    component.create(bootstrap).then(() => {
-      const view = component.element;
-      expect(view.textContent.trim()).toBe(model.content);
-      done();
-    }).catch(e => {
-      fail(e);
-      done();
+
+      component = StageComponent
+        .withResources('resources/elements/buttons/button/ej-button')
+        .inView(`<ej-button ${toKebabCase(key)}.bind="${key}"></ej-button>`)
+        .boundTo(model);
+
+      component.create(bootstrap).then(() => {
+        const view = component.element;
+        expect((<HTMLElement>view).attributes.getNamedItem(`${toKebabCase(key)}.bind`).value).toBe(key);
+        done();
+      }).catch(e => {
+        fail(e);
+        done();
+      });
     });
   });
 
-  it('displays the Content property statically', done => {
+  // it('binds the Content property', done => {
 
-    //console.log(`displays the Content property "${model.content}" statically`)
-    component = StageComponent
-      .withResources('resources/elements/buttons/button/ej-button')
-      .inView(`<ej-button content="${model.content}"></ej-button>`);
+  //   component = StageComponent
+  //     .withResources('resources/elements/buttons/button/ej-button')
+  //     .inView('<ej-button content.bind="content"></ej-button>')
+  //     .boundTo(model);
 
-    component.create(bootstrap).then(() => {
-      const view = component.element;
-      expect(view.textContent.trim()).toBe(`${model.content}`);
-      done();
-    }).catch(e => {
-      fail(e);
-      done();
-    });
-  });
+  //   component.create(bootstrap).then(() => {
+  //     const view = component.element;
+  //     console.log((<HTMLElement>view));
+  //     expect(view.textContent.trim()).toBe(model.content);
+  //     done();
+  //   }).catch(e => {
+  //     fail(e);
+  //     done();
+  //   });
+  // });
+
+//   it('displays the Content property statically', done => {
+
+//     console.log(`displays the Content property "${model.content}" statically`)
+//     component = StageComponent
+//       .withResources('resources/elements/buttons/button/ej-button')
+//       .inView(`<ej-button content="${model.content}"></ej-button>`);
+
+//     component.create(bootstrap).then(() => {
+//       const view = component.element;
+//       expect(view.textContent.trim()).toBe(`${model.content}`);
+//       done();
+//     }).catch(e => {
+//       fail(e);
+//       done();
+//     });
+//   });
 });
